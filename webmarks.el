5fbb702ba8e4aa802c9c57b19062f2570d2898e2
Richard Flood <rfcoder89@gmail.com>
2018-07-29 01:06:51 +0100

    loads bookmarks from json and can display them

---
+++ webmarks.el
@@ -0,0 +1,42 @@
+(require 'json)
+
+(setq webmarks-file "~/.emacs.d/webmarks.json")
+(setq webmarks--map (make-hash-table :test 'equal))
+
+(defun webmarks-reload ()
+  (interactive)
+  (let* ((json-array-type 'list)
+         (json-key-type 'string)
+         (j (json-read-file webmarks-file))
+         (loaded-webmarks 0))
+    (setq webmarks--map (make-hash-table :test 'equal))
+    (dolist (element j)
+      (setq loaded-webmarks (+ loaded-webmarks 1))
+      (puthash (car element) (cdr element) webmarks--map)
+      (message (car element))
+      (message (cdr element)))
+    (message (format "loaded %s webmarks" loaded-webmarks))))
+
+(defun webmarks-list-transformer (label)
+  (let ()
+	  (format
+	   "%s (%s)"
+	   label
+	   (propertize
+	    (gethash label webmarks--map)
+	    'face '(:foreground "yellow")))))
+
+;;;###autoload
+(defun webmarks-list ()
+  (interactive)
+  (ivy-read
+	 "Web bookmark: "
+	 webmarks--map
+	 :action (lambda (label)
+		         (browse-url (gethash label webmarks--map)))))
+
+(ivy-set-display-transformer
+ 'webmarks-list
+ 'webmarks-list-transformer)
+
+(provide 'webmarks)
